9982ed6a62a14b91b1eba975dabba596227163fd
MarinaSokolnikova <sokolnikovam92@gmail.com>
2023-03-26 13:58:00 +0300

    login+board

--- src/test/java/tests/BoardCreation.java
+++ src/test/java/tests/BoardCreation.java
@@ -1,33 +1,28 @@
 package tests;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class Login extends TestBase{
-    WebDriver wd;
+public class BoardCreation extends TestBase{
 
     @BeforeMethod
     public void preCondition(){
-        if (app.getUser().isLogged())
-        {
-            app.getUser().logout();
-        }
-    }
-
-    @Test
-    public void login1(){
         app.getUser().initLogin();
         app.getUser().pause(2000);
         app.getUser().fillLoginForm("sokolnikovam92@gmail.com", "FF22Kseriesqq!");
         app.getUser().submitLogin();
         app.getUser().pause(2000);
-
-        Assert.assertTrue(app.getUser().isLogged());
-
     }
 
-
+    @Test
+    public void boardCreation1(){
+        app.getBoard().initBoardCreationFromHeder();
+        app.getBoard().fillInBoardCreationForm();
+        app.scrollDownTheForm();
+        app.getBoard().pause(2000);
+        app.getBoard().submitBoardCreation();
+        app.getBoard().pause(2000);
+        app.getBoard().isCreated();
+    }
 }
